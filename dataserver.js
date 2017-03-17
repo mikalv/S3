501a7e48a430c4ad6c4c58cd6fac043bcf360490
Jonathan Gramain <jonathan.gramain@scality.com>
2017-03-17 11:42:18 -0700

    S3C-158 Use the new REST model in Arsenal to store and retrieve data blobs
    We spawn a new local REST server listening on 9991 ( or the value of
    config.dataDaemon.port) and we use a REST client module to communicate
    with the server.
    
    Additional changes:
    
     - cleanup: move S3METADATAPATH env var usage in mdserver.js
     - use parseRange from Arsenal
     - fix display of functional test on range: error case were displayed in place of "success" case

---
+++ dataserver.js
@@ -0,0 +1,29 @@
+'use strict'; // eslint-disable-line strict
+require('babel-core/register');
+
+const arsenal = require('arsenal');
+const config = require('./lib/Config.js').default;
+const logger = require('./lib/utilities/logger').logger;
+
+if (config.backends.data === 'file') {
+    /**
+     * Configure the file paths for data if using the file
+     * backend. If no path provided, uses data at the root of
+     * the S3 project directory.
+     */
+    const dataPath = process.env.S3DATAPATH ?
+              process.env.S3DATAPATH : `${__dirname}/localData`;
+    const restServer = new arsenal.network.rest.RESTServer(
+        { port: config.dataDaemon.port,
+          dataStore: new arsenal.storage.data.file.DataFileStore(
+              { dataPath, log: config.log }),
+          log: config.log });
+    restServer.setup(err => {
+        if (err) {
+            logger.error('Error initializing REST data server',
+                         { error: err });
+            return;
+        }
+        restServer.start();
+    });
+}
