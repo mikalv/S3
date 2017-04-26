scality/cloudserver

S3C-158 fix metadata class names
Adapt to changes done in https://github.com/scality/Arsenal/pull/235

lib/metadata/bucketfile/backend.js
@@ -8,7 +8,7 @@ import constants from '../../../constants';
 import config from '../../Config';
 
 const errors = arsenal.errors;
-const MetadataClient = arsenal.storage.metadata.client;
+const MetadataClient = arsenal.storage.metadata.MetadataFileClient;
 
 const METASTORE = '__metastore';
 const OPTIONS = { sync: true };

mikalv/S3

lib/metadata/bucketfile/backend.js
@@ -8,7 +8,7 @@ import constants from '../../../constants';
 import config from '../../Config';
 
 const errors = arsenal.errors;
-const MetadataFileClient = arsenal.storage.metadata.client;
+const MetadataFileClient = arsenal.storage.metadata.MetadataFileClient;
 
 const METASTORE = '__metastore';
 